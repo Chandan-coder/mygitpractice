fa952dfae156950bef034f730d31b3f6d85ea1b0
Chandan Kumar <chandansinghpatna19992@gmail.com>
2023-04-13 12:04:50 +0000

    added content from feature101

--- hello.java
+++ hello.java
@@ -3,10 +3,7 @@ class Hello
 Public static void main(String args[])
 {
 <<<<<<< HEAD
-System.out.println(“hello from local, adding additional contents”);
-=======
-System.out.println(“hello from central”);
->>>>>>> ddc5c6042686318aa9c5aabf9776c4e830497e6f
+System.out.println(“hello from feature101 branch”);
 }
 }
 
